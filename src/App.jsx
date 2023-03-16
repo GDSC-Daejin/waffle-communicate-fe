f24c5349350ea327580684859668d3479baaf4e9
urim <gse06044@naver.com>
2023-03-16 13:03:24 +0900

    layout :footer 수정

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,9 @@ function App() {
         <GlobalStyles />
         <TodoProvider>
           <Body />
+          <Footer />
         </TodoProvider>
       </ThemeProvider>
-      <Footer />
     </div>
   );
 }
--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -4,25 +4,15 @@ import styled from "styled-components";
 
 export const Logos = styled.div`
   display:flex;
-  justify-content:space-between;
-  font-size:20px;
-  font-weight:bold;
+  justify-content:center;
 `;
 
-export const Images = styled.image`
-  display:flex;
-  position:relative;
-  bottom:100px;
-`;
 
 function Footer (){
   return(
     <>
       <Logos>
-        <Images>
-          <img src={Logo} width="350" alt="GDSC로고" height="300"/>
-        </Images>
-        <p>James X Donny</p>
+          <img src={Logo} width="350" alt="GDSC로고" />
       </Logos>
     </>
   );
