56ae36f54112602d07cfee926a036bc18b5a0d59
urim <gse06044@naver.com>
2023-03-20 15:55:18 +0900

    layout:footer 디자인

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import Logo from "./img/Logo.png";
 import styled from "styled-components";
 
-export const Logos = styled.div`
+const Logos = styled.div`
   display:flex;
-  justify-content:center;
+  justify-content: space-between;
+  font-size:22px;
+  font-weight: bold;
+  padding:10px;
 `;
 
+const divs = styled.div`
+`;
 
 function Footer (){
   return(
     <>
       <Logos>
-          <img src={Logo} width="350" alt="GDSC로고" />
-          
+          <img src={Logo} alt="GDSC로고" width="150" height="130"/>
+          <p>Donny & James</p>
       </Logos>
-      <p>Donney & James</p>
     </>
   );
 }
